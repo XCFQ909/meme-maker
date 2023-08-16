0b2333dd4abec79ab89cde1cf33db6af9688ed92
XCFQ909 <chrisnam03@naver.com>
2023-08-16 09:28:56 +0900

    drawing a person

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ canvas.width = 800;
 canvas.height = 800;
 
 
-ctx.moveTo(100,100);
-ctx.lineTo(200,100)
-ctx.lineTo(200,200)
-ctx.lineTo(100,200)
-ctx.lineTo(100,100)
+ctx.fillRect(210 - 40, 170, 15, 200);
+ctx.fillRect(350 - 40, 170, 15, 200);
+ctx.fillRect(260 - 40, 170, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
 ctx.fill();
 
-ctx.fillRect(200,200,100,100)
-
-ctx.rect(300,300,100,100)
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(250 - 20, 95, 8, 0, Math.PI);
+ctx.arc(250 + 20, 95, 8, 0, Math.PI);
 ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(220,400, 15,200);
+ctx.fillRect(235 + 40,400, 15,200);
